ec22e86cf3d01e788a867a688e45fc17716ee6c6
verityostemu <anyelo888ra@gmail.com>
2026-07-09 00:08:48 -0400

    Add files via upload

---
+++ virus saline/end.bat
@@ -0,0 +1,13 @@
+@echo off
+title FIN DEL VIRUS FAKE
+color 0a
+echo ============================================
+echo        JAJA BRO, ERA TODO UNA BROMA
+echo ============================================
+echo Tu PC nunca estuvo en peligro.
+echo Todo fue un efecto visual GDI.
+echo Gracias por sobrevivir las 3 etapas 😭🔥
+echo ============================================
+@echo off
+shutdown -s -t 40 -c "antes de que muera tu pc lee lo que dice el .bat o la notepad tienes 40 segundos para usar tu pc"
+
